d72d4215c054bc1e79f6fc5b6ba2f2db004b0871
Madoha <madoha00@gmail.com>
2024-02-14 23:29:45 +0600

    Добавьте файлы проекта.

---
+++ asp.netDapperCrudPatrick/Controllers/SuperHeroController.cs
@@ -0,0 +1,56 @@
+﻿using asp.netDapperCrudPatrick.Models;
+using asp.netDapperCrudPatrick.Services;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace asp.netDapperCrudPatrick.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly SuperHeroService _heroService;
+        public SuperHeroController(SuperHeroService heroService)
+        {
+            _heroService = heroService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllSuperHeroes()
+        {
+            var heroes = await _heroService.GetAllHeroes();
+            return heroes != null ? Ok(heroes) : NoContent();
+        }
+
+        [HttpGet("{heroId}")]
+        public async Task<ActionResult<SuperHero>> GetHero(int heroId)
+        {
+            var hero = await _heroService.GetHero(heroId);
+            return hero != null ? Ok(hero) : NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateHero(SuperHero hero)
+        {
+            var createHero = await _heroService.CreateHero(hero);
+            return createHero != null ? Ok(createHero) : BadRequest();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero hero)
+        {
+            var updateHero = await _heroService.UpdateHero(hero);
+            return updateHero != null ? Ok(updateHero) : BadRequest();
+        }
+
+        [HttpDelete("{heroId}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int heroId)
+        {
+            var deleteHero = await _heroService.DeleteHero(heroId);
+            return deleteHero != null ? Ok(deleteHero) : BadRequest();
+        }
+
+    }
+}
---
+++ asp.netDapperCrudPatrick/Services/SuperHeroService.cs
@@ -0,0 +1,56 @@
+﻿using asp.netDapperCrudPatrick.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace asp.netDapperCrudPatrick.Services
+{
+    public class SuperHeroService
+    {
+        private readonly IConfiguration _configuration;
+        public SuperHeroService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private async Task<SqlConnection> GetConnection()
+        {
+            var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            await connection.OpenAsync();
+            return connection;
+        }
+
+        public async Task<IEnumerable<SuperHero>> GetAllHeroes()
+        {
+            using var connection = await GetConnection();
+            return await connection.QueryAsync<SuperHero>("select * from superheroes");
+        }
+
+        public async Task<SuperHero> GetHero(int heroId)
+        {
+            using var connection = await GetConnection();
+            return await connection.QueryFirstAsync<SuperHero>("select * from superheroes where id = @Id", new { Id = heroId });
+        }
+
+        public async Task<IEnumerable<SuperHero>?> CreateHero(SuperHero hero)
+        {
+            using var connection = await GetConnection();
+            var result = await connection.ExecuteAsync("insert into superheroes (name, firstname, lastname, place) values (@Name, @FirstName, @LastName, @Place)", hero);
+            return result > 0 ? await GetAllHeroes() : null;
+        }
+
+        public async Task<IEnumerable<SuperHero>?> UpdateHero(SuperHero hero)
+        {
+            using var connection = await GetConnection();
+            var result = await connection.ExecuteAsync("update superheroes set name = @Name, firstname = @FirstName, lastname = @LastName, place = @Place where id = @Id", hero);
+            return result > 0 ? await GetAllHeroes() : null;
+        }
+
+        public async Task<IEnumerable<SuperHero>> DeleteHero(int heroId)
+        {
+            using var connection = await GetConnection();
+            var result = await connection.ExecuteAsync("delete from superheroes where id = @Id", new { Id = heroId });
+            return result > 0 ? await GetAllHeroes() : null;
+        }
+    }
+}
